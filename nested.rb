7a44924975b821a00c868ce132602dc3f5888007
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-28 02:50:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,6 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -83,7 +78,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
 
 end
 
