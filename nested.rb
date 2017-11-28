9facce69add942456fbc2c644044496c17e97e79
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-28 02:37:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 languages = ""
-		 language = programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
